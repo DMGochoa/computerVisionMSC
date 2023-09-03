2b54728673b1c507b0b814824545eff9aab6ace9
diego moreno <diego.moreno@utp.edu.co>
2023-09-03 15:40:44 -0500

    Adding extra functionalities for the conicals

--- geometry/geometryP2.py
+++ geometry/geometryP2.py
@@ -4,10 +4,10 @@ from abc import ABC, abstractmethod
 
 class GeometryP2(ABC):
 
-    def __init__(self, array: list, nombre_elemento: str) -> None:
+    def __init__(self, array: list, element_name: str) -> None:
         super().__init__()
         self.array = np.array(array)
-        self.nombre_elemento = nombre_elemento
+        self.element_name = element_name
 
     @abstractmethod
     def plot(self):
--- geometry/pointGP2.py
+++ geometry/pointGP2.py
@@ -1,7 +1,7 @@
 """This module implements the PointGP2 class to represent a point in the projective plane P2.
 
 Diego A Moreno G
-Msc Student in Elctrical Engineering
+Msc Student in Electrical Engineering
 Universidad Tecnológica de Pereira
 20/08/2023
 """
@@ -12,30 +12,53 @@ import numpy as np
 from geometry.geometryP2 import GeometryP2
 
 class PointGP2(GeometryP2):
-    
-    def __init__(self, array, nombre):
-        super().__init__(array, nombre)
+    """The PointGP2 class represents a point in the projective plane P2.
+
+    Attributes:
+        vector (np.array): The homogeneous coordinates of the point.
+        element_name (str): The name of the point.
+    """
+
+    def __init__(self, array, name):
+        """Initializes the PointGP2 object with homogeneous coordinates and a name.
+
+        Args:
+            array (list or np.array): List or array of three elements that represent the point in the projective plane P2.
+                                      Should be in homogeneous coordinates [x, y, 1].
+            name (str): Name of the point.
+        """
+        super().__init__(array, name)
         if self.array[-1] != 1:
             self.vector = self.array / self.array[-1]
         else:
             self.vector = self.array
 
     def _crossProduct(self, array):
+        """Calculates the cross product of the point's vector with another vector.
+
+        Args:
+            array (np.array): The vector to cross with the point's vector.
+
+        Returns:
+            list: The resulting vector from the cross product.
+        """
         resultingVector = np.cross(self.vector.T, array)
         return resultingVector.tolist()
 
     def plot(self):
-        plt.scatter(self.vector[0],
-                    self.vector[1],
-                    marker='o',
-                    label=self.nombre_elemento)
-        plt.text(self.vector[0],
-                 self.vector[1],
-                 self.nombre_elemento,
-                 fontsize=16,
-                 ha='right')
+        """Plots the point on a 2D graph."""
+        plt.scatter(self.vector[0], self.vector[1], marker='o', label=self.element_name)
+        plt.text(self.vector[0], self.vector[1], self.element_name, fontsize=16, ha='right')
 
-    def calcularLinea(self, punto, nombre):
-        from geometry.lineGP2 import LineGP2  # Importación local
-        return LineGP2(self._crossProduct(punto.vector), nombre)
+    def calcularLinea(self, point, name):
+        """Calculates the line passing through this point and another point.
 
+        Args:
+            point (PointGP2): The other point through which the line passes.
+            name (str): The name of the resulting line.
+
+        Returns:
+            LineGP2: A LineGP2 object representing the line passing through the two points.
+        """
+        from geometry.lineGP2 import LineGP2
+        return LineGP2(self._crossProduct(point.vector), name)
